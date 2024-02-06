112d22f509c43aeb9cc6a22d48782c17c131dc5c
Jac-Luce <luce.jac92@gmail.com>
2024-02-06 17:46:03 +0100

    Aggiornamento script js

--- script.js
+++ script.js
@@ -1,66 +1,76 @@
 // https://striveschool-api.herokuapp.com/api/deezer/search?q=INSERISCIQUIUNAQUERY
-let eminemH2 = document.getElementById("eminem");
-eminemH2.classList.remove("d-none");
 
+/*Fetch per inserire gli album di Eminem*/
+
+let eminemDiv = document.getElementById("eminem");
+eminemDiv.classList.remove("d-none");
 
 fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
 .then((response) => response.json())
-.then((json) => console.log(json))
+.then((json) => {
+    let music = json.data
+    for (let i = 0; i < music.slice(0, 4).length; i++){
+        let element = music[i]
+        let eminemSection = document.getElementById('eminemSection');
+        let card = document.createElement('div');
+        card.classList=['card'];
+        let cardBody = document.createElement('div');
+        cardBody.classList = ['card-body p-0'];
+        let image = document.createElement('img');
+        image.src = element.album.cover;
+        cardBody.appendChild(image);
+        card.appendChild(cardBody);
+        eminemSection.appendChild(card);
+    }
+})
 .catch((err) => console.log("Error detected: ", err));
 
+/*Fetch per inserire gli album dei Metallica */
 
-let eminemSection = document.getElementById('eminemSection');
-let card = document.createElement('div');
-card.classList=['card'];
-let cardBody = document.createElement('div');
-cardBody.classList = ['card-body p-0'];
-let image = document.createElement('img');
-image.src = json.album.cover;
-cardBody.appendChild(image);
-card.appendChild(cardBody);
-eminemSection.appendChild(card);
+let metallicaDiv = document.getElementById("metallica");
+metallicaDiv.classList.remove("d-none");
 
-
-
-
-
-//edoardo midali  su youtube per js
-
-/*
-function artista(){
-    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}
-artista();
-function creaCard (artista, json) {
-    let container = document.getElementById(artista);
-    container.classList=['d-flex flex-column'];
-    let div = document.getElementById(artista+'Section');
-
-    let card = document.createElement('div');
-    card.classList = ['card'];
-    let cardBody = document.createElement('div');
-    cardBody.classList = ['card-body p-0'];
-    let image = document.createElement('img');
-    image.src = json.album.cover;
-    cardBody.appendChild(image);
-    card.appendChild(cardBody);
-    div.appendChild(card);
-}
-creaCard(eminem, json);
-
-
-function album() {
-    const artisti = ['eminem', 'metallica', 'queen'];
-    for (const artista of artisti) {
-        fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=${artista}")
-        .then(response => response.json())
-        .then(json => {
-            for (let i = 0; i < 4; ++i) {
-                console.log(json.data[i])
-                creaCard(artista, json.data[i]);
-            }
-        });
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica")
+.then((response) => response.json())
+.then((json) => {
+    let music = json.data
+    for (let i = 0; i < music.slice(0, 4).length; i++){
+        let element = music[i]
+        let metallicaSection = document.getElementById('metallicaSection');
+        let card = document.createElement('div');
+        card.classList=['card'];
+        let cardBody = document.createElement('div');
+        cardBody.classList = ['card-body p-0'];
+        let image = document.createElement('img');
+        image.src = element.album.cover;
+        cardBody.appendChild(image);
+        card.appendChild(cardBody);
+        metallicaSection.appendChild(card);
     }
-} */
+})
+.catch((err) => console.log("Error detected: ", err));
+
+/*Fetch per inserire gli album dei Queen */
+
+let queenDiv = document.getElementById("queen");
+queenDiv.classList.remove("d-none");
+
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen")
+.then((response) => response.json())
+.then((json) => {
+    let music = json.data
+    for (let i = 0; i < music.slice(0, 4).length; i++){
+        let element = music[i]
+        let queenSection = document.getElementById('queenSection');
+        let card = document.createElement('div');
+        card.classList=['card'];
+        let cardBody = document.createElement('div');
+        cardBody.classList = ['card-body p-0'];
+        let image = document.createElement('img');
+        image.src = element.album.cover;
+        cardBody.appendChild(image);
+        card.appendChild(cardBody);
+        queenSection.appendChild(card);
+    }
+})
+.catch((err) => console.log("Error detected: ", err));
